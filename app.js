2c06f71c37af44ca460b08c42519b224d414eab2
Jules Duro <ourshieldoath@gmail.com>
2017-11-07 22:12:26 -0800

    Created MVP

---
+++ app.js
@@ -0,0 +1,54 @@
+'use strict'
+
+var userName = prompt("Hello! May I ask for your name, please?");
+alert('Welcome, ' + userName + '!');
+
+var likesPineapplePizza = prompt('Does Jules like pineapple pizza?');
+if (likesPineapplePizza.toLowerCase() === 'y' ||
+    likesPineapplePizza.toLowerCase() === 'yes'){
+      alert('Correct!');
+    }else if(likesPineapplePizza.toLowerCase() == 'n' ||
+             likesPineapplePizza.toLowerCase() == 'no'){
+               alert('Actually, that\'s Jules\' favorite type of Pizza!');
+            }
+console.log("Does Jules like pizza", likesPineapplePizza);
+
+var favoriteConsole = prompt('Do you think Jules\' favorite gaming console is the PS4?');
+if (favoriteConsole.toLowerCase() === 'y' ||
+    favoriteConsole.toLowerCase() === 'yes'){
+      alert('Jules is not a dirty console peasant! PC Master Race forever!');
+    }else if(favoriteConsole.toLowerCase() == 'n' ||
+             favoriteConsole.toLowerCase() == 'no'){
+               alert('Correct! PC Master Race! Praise Gaben!')
+             }
+console.log("Favorite gaming console", favoriteConsole);
+
+var isJulesNice = prompt('Would Jules\' save you if you were being attacked by a bear?');
+if (isJulesNice.toLowerCase() === 'y' ||
+    isJulesNice.toLowerCase() === 'yes'){
+      alert('He would defintely save you, ' + userName + '!');
+    }else if(isJulesNice.toLowerCase() === 'n' ||
+             isJulesNice.toLowerCase() === 'no'){
+               alert("No! What do you take him for? A coward?!");
+             }
+console.log("Will prevent bear attack", isJulesNice);
+
+var likesRain = prompt('Does Jules like rain?');
+if (favoriteConsole.toLowerCase() === 'y' ||
+   favoriteConsole.toLowerCase() === 'yes'){
+     alert('Jules hates rainy days!');
+   }else if(favoriteConsole.toLowerCase() == 'n' ||
+            favoriteConsole.toLowerCase() == 'no'){
+              alert('Correct! Sunny days are much better.');
+            }
+console.log("Does Jules like rain", likesRain);
+
+var isCatPerson = prompt('Jules is totally a cat person! Don\'t you think so,  ' + userName + '?');
+if (isCatPerson.toLowerCase() === 'y' ||
+   isCatPerson.toLowerCase() === 'yes'){
+     alert('As much as he likes cats, he likes dogs a little bit more!');
+   }else if(isCatPerson.toLowerCase() === 'n' ||
+            isCatPerson.toLowerCase() === 'no'){
+              alert("Yup - he is actually allergic to cats!");
+            }
+console.log("Is Jules a cat person?", isCatPerson);
