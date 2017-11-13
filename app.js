b2099621370d640245cf9733ac37597e12d594d9
Jules Duro <ourshieldoath@gmail.com>
2017-11-12 22:07:35 -0800

    CSS / JS modified

--- app.js
+++ app.js
@@ -3,117 +3,120 @@
 var tally = 0;
 
 var userName = prompt('Hello! May I ask for your name, please?');
-function userNameFunction()
-{
 alert('Welcome, ' + userName + '!');
 
-var likesPineapplePizza = prompt('Does Jules like pineapple pizza?').toLowerCase();
-if (likesPineapplePizza === 'y' || likesPineapplePizza === 'yes'){
-  alert('Correct!');
-  tally++;
-} else if(likesPineapplePizza === 'n' || likesPineapplePizza === 'no'){
-  alert('Actually, that\'s Jules\' favorite type of Pizza!');
-}
-console.log('Does Jules like pizza', likesPineapplePizza);
-}
-userNameFunction();
-
-function favoriteConsoleFunction() {
-var favoriteConsole = prompt('Do you think Jules\' favorite gaming console is the PS4?').toLowerCase();
-if (favoriteConsole === 'y' || favoriteConsole === 'yes'){
-  alert('Nope - PC\'s are much better for gaming.');
-} else if(favoriteConsole === 'n' || favoriteConsole === 'no'){
-  alert('Correct! PC Master Race!');
-  tally++;
-}
-console.log('Favorite gaming console', favoriteConsole);
-}
-favoriteConsoleFunction();
-
-
-var isJulesNice = prompt('Would Jules\' save you if you were being attacked by a bear?').toLowerCase();
-function isJulesNiceFunction() {
-if (isJulesNice === 'y' || isJulesNice === 'yes'){
-  alert('He would defintely save you, ' + userName + '!');
-  tally++;
-} else if(isJulesNice === 'n' || isJulesNice === 'no'){
-  alert('No! What do you take him for? A coward?!');
-}
-console.log('Will prevent bear attack', isJulesNice);
-}
-isJulesNiceFunction();
-
-var likesRain = prompt('Does Jules like rain?').toLowerCase();
-function likesRainFunction () {
-if (likesRain === 'y' ||
-    likesRain === 'yes'){
-  alert('Jules hates rainy days!');
-} else if(likesRain === 'n' ||
-         likesRain === 'no'){
-  alert('Correct! Sunny days are much better.');
-  tally++;
-}
-console.log('Does Jules like rain', likesRain);
-}
-likesRainFunction ();
-
-var isCatPerson = prompt('Jules is totally a cat person! Don\'t you think so,  ' + userName + '?').toLowerCase();
-function isCatPersonFunction () {
-if (isCatPerson === 'y' ||
-    isCatPerson === 'yes'){
-  alert('As much as he likes cats, he likes dogs a little bit more!');
-} else if(isCatPerson === 'n' ||
-          isCatPerson === 'no'){
-  alert('Yup - he is actually allergic to cats!');
-  tally++;
-}
-console.log('Is Jules a cat person?', isCatPerson);
-}
-isCatPersonFunction();
-
-  function guessNumFunction() {
-for (var i = 1; i <= 4; i++){
-  do
-    var guessNum = parseInt(prompt('What\'s my favorite number?\nHint: It\'s between 1 and 50.'));
-  while(isNaN(guessNum));
-  if (i === 4){
-    alert('Sorry, but my favorite number is ' + 14 + '.');
-  } else if (guessNum > 14){
-    alert('Guess lower!');
-  } else if(guessNum < 14){
-    alert('Guess higher!');
-  } else if(guessNum === 14){
-    alert('You got it!');
+function pinePizzaQ() {
+  var likesPineapplePizza = prompt('Does Jules like pineapple pizza?').toLowerCase();
+  if (likesPineapplePizza === 'y' || likesPineapplePizza === 'yes'){
+    alert('Correct!');
     tally++;
-    break;
+  } else if(likesPineapplePizza === 'n' || likesPineapplePizza === 'no'){
+    alert('Actually, that\'s Jules\' favorite type of Pizza!');
   }
+  console.log('Does Jules like pizza', likesPineapplePizza);
 }
-}
-guessNumFunction();
-console.log('user guessd number', guessNum);
+pinePizzaQ();
 
-function whatStateFunction () {
-var whatState;
-var counter = 0;
-var listStates = ['california', 'new york', 'louisiana', 'oregon'];
-outer_loop:
-while(counter < 6){
-  whatState = prompt('What State would I visit?').toLowerCase();
-  if (listStates.includes(whatState)){
-    alert('Congrats! You guessed right!');
+function faveConsole() {
+  var favoriteConsole = prompt('Do you think Jules\' favorite gaming console is the PS4?').toLowerCase();
+  if (favoriteConsole === 'y' || favoriteConsole === 'yes'){
+    alert('Nope - PC\'s are much better for gaming.');
+  } else if(favoriteConsole === 'n' || favoriteConsole === 'no'){
+    alert('Correct! PC Master Race!');
     tally++;
-    break outer_loop;
-  } else{
-    alert('Incorrect.');
-    counter++;
   }
-} if (counter === 6){
-  alert('Sorry, but I would like to visit ' + listStates);
+  console.log('Favorite gaming console', favoriteConsole);
 }
-console.log('State guessed', whatState);
-console.log('Tally count', tally);
+faveConsole();
+
+
+
+function julesIsNice() {
+  var isJulesNice = prompt('Would Jules\' save you if you were being attacked by a bear?').toLowerCase();
+  if (isJulesNice === 'y' || isJulesNice === 'yes'){
+    alert('He would defintely save you, ' + userName + '!');
+    tally++;
+  } else if(isJulesNice === 'n' || isJulesNice === 'no'){
+    alert('No! What do you take him for? A coward?!');
+  }
+  console.log('Will prevent bear attack', isJulesNice);
 }
-whatStateFunction();
+julesIsNice();
+
+
+function likesRain() {
+  var likesRain = prompt('Does Jules like rain?').toLowerCase();
+  if (likesRain === 'y' ||
+      likesRain === 'yes'){
+    alert('Jules hates rainy days!');
+  } else if(likesRain === 'n' ||
+           likesRain === 'no'){
+    alert('Correct! Sunny days are much better.');
+    tally++;
+  }
+  console.log('Does Jules like rain', likesRain);
+}
+likesRain ();
+
+
+function isCatPerson() {
+  var isCatPerson = prompt('Jules is totally a cat person! Don\'t you think so,  ' + userName + '?').toLowerCase();
+  if (isCatPerson === 'y' ||
+      isCatPerson === 'yes'){
+    alert('As much as he likes cats, he likes dogs a little bit more!');
+  } else if(isCatPerson === 'n' ||
+            isCatPerson === 'no'){
+    alert('Yup - he is actually allergic to cats!');
+    tally++;
+  }
+  console.log('Is Jules a cat person?', isCatPerson);
+}
+isCatPerson();
+
+function guessNum() {
+  for (var i = 1; i <= 4; i++){
+    do
+      var guessNum = parseInt(prompt('What\'s my favorite number?\nHint: It\'s between 1 and 50.'));
+    while(isNaN(guessNum));
+    if (i === 4){
+      alert('Sorry, but my favorite number is ' + 14 + '.');
+    } else if (guessNum > 14){
+      alert('Guess lower!');
+    } else if(guessNum < 14){
+      alert('Guess higher!');
+    } else if(guessNum === 14){
+      alert('You got it!');
+      tally++;
+      break;
+    }
+  }
+  console.log('user guessed number', guessNum);
+}
+guessNum();
+
+
+function whatState () {
+  var whatState;
+  var counter = 0;
+  var listStates = ['california', 'new york', 'louisiana', 'oregon'];
+  outer_loop:
+  while(counter < 6){
+    whatState = prompt('What State would I visit?').toLowerCase();
+    if (listStates.includes(whatState)){
+      alert('Congrats! You guessed right!');
+      tally++;
+      break outer_loop;
+    } else{
+      alert('Incorrect.');
+      counter++;
+    }
+  } if (counter === 6){
+    alert('Sorry, but I would like to visit ' + listStates);
+  }
+  console.log('State guessed', whatState);
+  console.log('Tally count', tally);
+}
+whatState();
 
 
 if (tally === 0 || tally === 1){
